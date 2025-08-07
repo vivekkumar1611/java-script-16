7fbdf5e5a4ba1269f277e929de39ac99e6e2db30
vivekkumar1611 <k.vivekk1@gmail.com>
2025-08-08 00:02:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+// day 01
+
+var x=5;
+
+x="hello";
+
+var x=true;
+console.log(x);
+
+{
+    let a=10;
+
+console.log(a);
+}
+
+let data1 = 42;
+let data2="42";
+ let dat3;
+console.log(typeof data1);
+console.log(typeof data2);
+console.log(typeof data3);
+
+let userinput=prompt ("enter your age:");
+console.log(typeof userinput);
+
+const name="john"
+
+console.log(name)
+
+// 7/8/2025
+
+// console.log(10+10);
+// console.log(10-10);
+// console.log(10*10);
+// console.log(10/10);
+// console.log(10%11);
+// console.log(10**10);
+
+// let int=10;
+// int=int++
+// console.log(int);
+
+// let numbers=20
+// let numbers1=++numbers
+// console.log(numbers,numbers1)
+
+// let a1=12
+// let b1=++a1
+// let c1=++b1
+// console.log(a1,b1,c1)
+
+// let d1=1
+// let e1=--d1
+// let f1=++e1
+// console.log(d1,e1,f1)
+
+// let num1=20
+// let num2=num1++ + --num1
+// console.log(num1,num2)
