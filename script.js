c7bcfbf5c0c33d794c16469b0d9ff8eef703727e
vivekkumar1611 <k.vivekk1@gmail.com>
2025-08-11 00:24:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-// day 01
+// // day 01
 
-var x=5;
+// var x=5;
 
-x="hello";
+// x="hello";
 
-var x=true;
-console.log(x);
+// var x=true;
+// console.log(x);
 
-{
-    let a=10;
+// {
+//     let a=10;
 
-console.log(a);
-}
+// console.log(a);
+// }
 
-let data1 = 42;
-let data2="42";
- let dat3;
-console.log(typeof data1);
-console.log(typeof data2);
-console.log(typeof data3);
+// let data1 = 42;
+// let data2="42";
+//  let dat3;
+// console.log(typeof data1);
+// console.log(typeof data2);
+// console.log(typeof data3);
 
-let userinput=prompt ("enter your age:");
-console.log(typeof userinput);
+// let userinput=prompt ("enter your age:");
+// console.log(typeof userinput);
 
-const name="john"
+// const name="john"
 
-console.log(name)
+// console.log(name)
 
-// 7/8/2025
+// // 7/8/2025
 
 // console.log(10+10);
 // console.log(10-10);
@@ -57,3 +57,49 @@ console.log(name)
 // let num1=20
 // let num2=num1++ + --num1
 // console.log(num1,num2)
+
+// 10/08/2025
+
+let x = 5;
+let y = x++ + ++x;
+console.log(x, y);
+
+let total=50;
+total +=30;
+total /=4;
+console.log(total)
+
+console.log(10 == "10");
+console.log(10 === "10");
+
+let age=25;
+console.log(age >=25);
+let experience=3;
+console.log( experience >=3);
+console.log(age >=25 && experience >=3 );
+
+console.log(typeof(5 + "5"));
+
+console.log(Boolean(0));
+console.log(Boolean("false"));
+console.log(Boolean([]));
+
+let num="123"
+let add=10;
+console.log(num+add);
+console.log(Number(num)+add);
+
+console.log(Boolean({}));
+console.log(Boolean(null));
+
+let one="Hello";
+let two="Students";
+let result= one +" "+ two;
+console.log(result);
+let title=`final: ${one} ${two}`;
+console.log(title);
+
+let a="10";
+let b=10;
+console.log(a+b);
+console.log(Number(a)+b);
